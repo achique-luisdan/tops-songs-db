f1fe0183536c07680eeff4cd18ced5b0a3322126
achique-luisdan <achique.luisdan@gmail.com>
2023-07-29 02:34:15 -0400

    refactor: :recycle: Consulta Top Canciones de la Semana con información del puesto anterior de las canciones

--- src/stored-procedures.sql
+++ src/stored-procedures.sql
@@ -1,21 +1,24 @@
 -- FUNCIÓN: Consulta Top Canciones de la Semana (fecha_inicio, fecha_fin)
 -- SELECT * FROM spotify.sp_top_songs_week('2022-01-07', '2022-01-13');
-
-CREATE OR REPLACE FUNCTION spotify.sp_top_songs_week (
-    IN start_date_week DATE,
-    IN end_date_week DATE,
-    OUT rank BIGINT,
-    OUT track_name VARCHAR,
-    OUT artist_names TEXT,
-    OUT record_company_name VARCHAR,
-    OUT streams INTEGER
+-- DROP FUNCTION IF EXISTS spotify.sp_top_songs_week(date, date);
+CREATE OR REPLACE FUNCTION spotify.sp_top_songs_week(
+	IN start_date_week date,
+	IN end_date_week date,
+	OUT song_id integer,
+	OUT rank bigint,
+	OUT track_name character varying,
+	OUT artist_names text,
+	OUT record_company_name character varying,
+	OUT streams integer
 )
-RETURNS SETOF RECORD AS $BODY$
+RETURNS SETOF record
+    LANGUAGE 'plpgsql'
+AS $BODY$
 DECLARE
     reg RECORD;
 BEGIN
     FOR REG IN
-        SELECT ROW_NUMBER () OVER () AS rank, s.track_name, string_agg (a.name, ', ') AS artist_names , rc.name AS record_company_name, ws.streams FROM core.songs s
+        SELECT s.id, ROW_NUMBER () OVER () AS rank, s.track_name, string_agg (a.name, ', ') AS artist_names , rc.name AS record_company_name, ws.streams FROM core.songs s
         JOIN core.record_companies rc ON (s.record_company_id = rc.id)
         JOIN core.song_artist sa ON (s.id = sa.song_id)
         JOIN core.artists a ON (a.id = sa.artist_id)
@@ -24,6 +27,7 @@ BEGIN
         GROUP BY s.id, s.track_name, rc.name,  ws.streams, ws.end_date
         ORDER BY ws.streams DESC
      LOOP
+       song_id := reg.id;
        rank := reg.rank;
        track_name := reg.track_name;
        artist_names := reg.artist_names;
@@ -33,8 +37,63 @@ BEGIN
     END LOOP;
     RETURN;
 END
-$BODY$ LANGUAGE 'plpgsql';
+$BODY$;
 
+-- FUNCIÓN: Consulta Top Canciones de la Semana (fecha_inicio, fecha_fin) retorna más información (puesto previo, movimiento)
+-- SELECT * FROM spotify.sp_top_songs_week('2022-01-07', '2022-01-13');
+-- DROP FUNCTION IF EXISTS spotify.sp_top_songs_week_full(date, date);
+
+CREATE OR REPLACE FUNCTION spotify.sp_top_songs_week_full(
+    IN start_date_week date,
+    IN end_date_week date,
+    OUT song_id integer,
+    OUT rank bigint,
+    OUT track_name character varying,
+    OUT artist_names text,
+    OUT record_company_name character varying,
+    OUT streams integer,
+    OUT previous bigint,
+    OUT move bigint,
+    OUT type_move TEXT
+)
+RETURNS SETOF record
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+    reg RECORD;
+    start_date_last_week DATE;
+    end_date_last_week DATE;
+BEGIN
+SELECT
+    TO_CHAR(start_date_week - CAST ('7 days' AS INTERVAL), 'YYYY-MM-DD'),
+    TO_CHAR(end_date_week - CAST ('7 days' AS INTERVAL), 'YYYY-MM-DD')
+    INTO start_date_last_week, end_date_last_week;
+    FOR REG IN
+        SELECT w.*, lw.rank AS previous, lw.rank -w.rank AS move,
+            CASE
+                WHEN lw.rank - w.rank > 0 THEN 'UP'
+                WHEN lw.rank -w.rank < 0 THEN 'DOWN'
+                WHEN lw.rank -w.rank = 0 THEN 'STOP'
+                ELSE 'UNKNOWN'
+            END AS type_move
+        FROM spotify.sp_top_songs_week(start_date_week, end_date_week) w
+        LEFT JOIN spotify.sp_top_songs_week(start_date_last_week, end_date_last_week) lw ON (w.song_id = lw.song_id)
+        ORDER BY w.streams DESC
+     LOOP
+       song_id := reg.song_id;
+       rank := reg.rank;
+       track_name := reg.track_name;
+       artist_names := reg.artist_names;
+       record_company_name := reg.record_company_name;
+       streams := reg.streams;
+       previous := reg.previous;
+       move := reg.move;
+       type_move:= reg.type_move;
+       RETURN NEXT;
+    END LOOP;
+    RETURN;
+END
+$BODY$;
 
 -- PROCESO ALMACENADO: Busca la semana dado una fecha de entrada
 -- CALL spotify.sp_find_week_by_date('2022-06-07', NULL, NULL);
